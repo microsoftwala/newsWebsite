4e1ec3dae538dd5eb068d4b1b16cf18bba8eafd0
microsoftwala <124462509+microsoftwala@users.noreply.github.com>
2023-12-30 21:16:58 +0530

    Add files via upload

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
-import newspaperImage from './news.jpeg';
+import newspaperImage from '../../img/news.jpeg';
 
 function Home(props) {
   const backgroundImageStyle = {
